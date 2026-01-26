3c221190b70a46bfe77d9aa1a79656fe8750e680
Brian G <brigeo987@gmail.com>
2026-01-25 21:43:52 -0500

    working with the fccamp tutorial

--- lab-1-react-quiz/Lab One Quiz/src/main.jsx
+++ lab-1-react-quiz/Lab One Quiz/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById("root"))
+const reactElement = createElement("h1", null,"Hello Y'all")
+
+console.log(reactElement)
+
+root.render(
+  reactElement
 )
